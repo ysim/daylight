45652a442e69c1a07485ccfff6279f29f754e6e5
Yi Qing Sim <devnull@yiqingsim.net>
2017-05-24 21:37:21 +0200

    web/main.go: added for example

---
+++ web/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func GetClientIP(r *http.Request) (string, string) {
+	return r.RemoteAddr, r.Header.Get("X-Forwarded-For")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	remoteAddr, originIP := GetClientIP(r)
+	fmt.Fprintf(w, "RemoteAddr: %s, X-Forwarded-For: %s", remoteAddr, originIP)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
+}
